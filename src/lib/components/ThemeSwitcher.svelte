21868619b1c7baa5370d9a3fd6bf4d2faa77e01c
EugeneDraitsev <ddrrai@gmail.com>
2024-12-08 00:16:52 +0100

    Add expand canvas logic. Use icons for theme swither.

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte';
+
   const LOCAL_STORAGE_KEY = 'cv-theme';
   const THEME_ATTRIBUTE = 'data-theme';
 
@@ -19,14 +21,21 @@
   });
 </script>
 
-<div class="fixed top-4 right-4 text-2xl">
-  {#if theme === 'light'}
-    <button class=" cursor-pointer" onclick={() => setNewTheme('dark')}>
-      🌚
-    </button>
-  {:else if theme === 'dark'}
-    <button class="cursor-pointer" onclick={() => setNewTheme('light')}>
-      🌞
-    </button>
-  {/if}
-</div>
+<!-- TODO: Use bg-base-300/50 -->
+<nav class="fixed top-0 w-full">
+  <div
+    class="text-2xl text-identifier max-w-[1300px] mx-auto py-4 px-6 flex justify-end"
+  >
+    {#if theme === 'light'}
+      <button class="cursor-pointer h-6" onclick={() => setNewTheme('dark')}>
+        <Icon icon="mdi:weather-night" />
+      </button>
+    {:else if theme === 'dark'}
+      <button class="cursor-pointer h-6" onclick={() => setNewTheme('light')}>
+        <Icon icon="mdi:weather-sunny" />
+      </button>
+    {:else}
+      <div class="h-6"></div>
+    {/if}
+  </div>
+</nav>
--- src/lib/components/three/App.svelte
+++ src/lib/components/three/App.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
   import { Canvas } from '@threlte/core';
+  import Icon from '@iconify/svelte';
+
   import Scene from './Scene.svelte';
+
+  let expanded = $state(false);
 </script>
 
-<div class="app relative h-[500px] overflow-hidden">
+<div
+  class="app relative overflow-hidden transition-all ease-in-out duration-200"
+  style="height: {expanded ? '100dvh' : '500px'};"
+>
   <Canvas>
     <Scene />
   </Canvas>
+
+  <div class="hidden lg:block relative mx-auto max-w-[1200px] w-full">
+    <Icon
+      class="absolute bottom-6 left-2 w-8 h-8 m-2 text-identifier/90 cursor-pointer transition-transform ease-in-out duration-200 {expanded
+        ? ''
+        : 'rotate-180'}"
+      icon="icons8:chevron-up-round"
+      onclick={() => (expanded = !expanded)}
+    />
+  </div>
+
   <div
-    class="absolute bottom-0 left-0 w-full h-[100px] bg-gradient-to-t from-base-100 to-transparent pointer-events-none"
+    class="absolute bottom-0 left-0 w-full h-[100px] bg-gradient-to-t from-base-100 to-transparent pointer-events-none pointer-events-none"
   ></div>
 </div>
