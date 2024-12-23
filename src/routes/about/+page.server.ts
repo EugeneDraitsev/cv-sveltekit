33cfd5cf727469555dda5f62679a9619b6008363
EugeneDraitsev <ddrrai@gmail.com>
2024-12-23 02:13:35 +0100

    Use .data extension for lighthouse data

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -7,7 +7,7 @@ export const load: PageServerLoad = async ({ fetch, url, setHeaders }) => {
   });
 
   // Construct the absolute URL by combining the origin and the relative path
-  const absoluteUrl = new URL('/lighthouse-report.html', url.origin).href;
+  const absoluteUrl = new URL('/lighthouse-report.data', url.origin).href;
   const response = await fetch(absoluteUrl);
   const lighthouseData = await response.text();
 
