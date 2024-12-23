4dd83e2188cae5a84b1626708e1c31b13fe7f127
EugeneDraitsev <ddrrai@gmail.com>
2024-12-23 02:11:22 +0100

    Add new favicon. Update lighthouse data. Fix minor mobile rendering issue

--- src/lib/components/StarRating.svelte
+++ src/lib/components/StarRating.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-  export let rating = 0;
+  const { rating } = $props();
+  let ref = $state<HTMLSpanElement>();
+  const width = $derived(ref?.getBoundingClientRect()?.width ?? 83);
 </script>
 
-<div class="relative flex text-[20px]">
+<div class="relative flex text-[20px] indent-0" bind:this={ref}>
   <span class="text-identifier/15">★★★★★</span>
 
   <span
     class="text-identifier absolute top-0 left-0 overflow-hidden"
-    style="width: {(17 * rating) / 2 - 1}px;"
+    style="width: {(width * rating) / 10}px;"
   >
     {#each Array(Math.round(rating / 2)) as _, i}
       ★
